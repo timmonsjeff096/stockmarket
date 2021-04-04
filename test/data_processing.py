2196b8ccf1caa4e79f264b8597c1172da233b5fb
jeff <timmonsjeff096@gmail.com>
2021-04-04 02:09:17 -0400

    moved methods into data_processing.py removed logger temporarily(works kinda) removed all code from rfr.py

--- test/data_processing.py
+++ test/data_processing.py
@@ -2,37 +2,68 @@ import os
 import numpy as np
 import pandas as pd
 import numerapi
-import log
-
-# Initialize Numerai's API (important)
-napi = numerapi.NumerAPI(verbosity="info")
-# Directory to save the data to (set this yourself)
-out = "C:\Python\Projects\stockmarket\data"
+import json
+import logging
 
 
-def download_current_data(directory: str):
+def setup_config() -> dict:
+    with open("config.json") as cfg:
+        myjson = json.load(cfg)
+        cfg_values = {"output_directory": myjson.get("output_directory"),
+                      "data_directory": myjson.get("data_directory"),
+                      "log_directory": myjson.get("log_directory")}
+        json.dumps(cfg_values)
+        return cfg_values
+
+
+def setup_logging(log_dir):
+    logger = logging.getLogger('sgd_svrg')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    fh = logging.FileHandler(log_dir + '\stocklog.log')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+
+def set_pd_option():
     """
-    Downloads the data for the current round
-    :param directory: The path to the directory where the data needs to be saved
+    Set pandas dataframe output configuration
     """
+    pd.set_option('display.max_rows', 25)
+    pd.set_option('display.min_rows', 25)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 1000)
+    pd.set_option('display.colheader_justify', 'center')
+    pd.set_option('display.precision', 2)
+
+
+def download_current_data(data_dir):
+    """
+    Download latest numerai data from numerapi
+    :return: self
+    """
+    # Initialize Numerai's API (important)
+    napi = numerapi.NumerAPI(verbosity="info")
     current_round = napi.get_current_round()
-    full_path = f'{directory}/numerai_dataset_{napi.get_current_round()}'
-    if os.path.isdir(f'{directory}/numerai_dataset_{current_round}/'):
-        log.logger.info(msg=f"You already have the newest data. Current round is: {current_round}")
+    full_path = f'{data_dir}/numerai_dataset_{napi.get_current_round()}'
+    if os.path.isdir(full_path):
+        # df_logger(msg=f"You already have the newest data. Current round is: {current_round}")
+        print()
     else:
-        napi.download_current_dataset(dest_path=directory, unzip=True)
+        napi.download_current_dataset(dest_path=data_dir, unzip=True)
 
     # Throw out files we don't need + old files
-    if os.path.exists(out + '\\numerai_dataset_' + str(current_round) + '.zip'):
-        os.remove(out + '\\numerai_dataset_' + str(current_round) + '.zip')
-    if os.path.exists(out + '\\numerai_dataset_' + str(current_round - 1) + '\\numerai_tournament_data.csv'):
-        os.remove(out + '\\numerai_dataset_' + str(current_round - 1) + '\\numerai_tournament_data.csv')
-    if os.path.exists(out + '\\numerai_dataset_' + str(current_round - 1) + '\\numerai_training_data.csv'):
-        os.remove(out + '\\numerai_dataset_' + str(current_round - 1) + '\\numerai_training_data.csv')
-    if os.path.exists(out + '\\numerai_dataset_' + str(current_round - 1)):
-        os.removedirs(out + '\\numerai_dataset_' + str(current_round - 1))
-    if os.path.exists(full_path + 'analysis_and_tips.ipynb'):
-        os.remove(full_path + 'analysis_and_tips.ipynb')
+    if os.path.exists(full_path + '.zip'):
+        os.remove(full_path + '.zip')
+    if os.path.exists(data_dir + 'numerai_dataset_' + str(current_round - 1) +
+                      'numerai_tournament_data.csv'):
+        os.remove(data_dir + 'numerai_dataset_' + str(current_round - 1) + 'numerai_tournament_data.csv')
+    if os.path.exists(data_dir + 'numerai_dataset_' + str(current_round - 1) + 'numerai_training_data.csv'):
+        os.remove(data_dir + 'numerai_dataset_' + str(current_round - 1) + 'numerai_training_data.csv')
+    if os.path.exists(data_dir + 'numerai_dataset_' + str(current_round - 1)):
+        os.removedirs(data_dir + 'numerai_dataset_' + str(current_round - 1))
+    if os.path.exists(full_path + 'dataanalysis_and_tips.ipynb'):
+        os.remove(full_path + 'dataanalysis_and_tips.ipynb')
     if os.path.exists(full_path + 'example_model.py'):
         os.remove(full_path + 'example_model.py')
     if os.path.exists(full_path + 'example_model.r'):
@@ -40,24 +71,24 @@ def download_current_data(directory: str):
     if os.path.exists(full_path + 'example_predictions.csv'):
         os.remove(full_path + 'example_predictions.csv')
     else:
-        log.logger.info(msg=f"Latest data downloaded successfully. Current round is: {current_round}")
+        # df_logger(msg=f"Latest data downloaded successfully. Current round is: {current_round}")
         return
 
 
-def load_data(directory: str, reduce_memory: bool = True) -> tuple:
+def load_data(data_dir, reduce_memory: bool = True) -> tuple:
     """
     Get data for current round in pandas dataframe
-    :param reduce_memory: Reduce all features to 32-bit floats
-    :param directory: The path to the directory where the data needs to be saved
     :return: A tuple containing the datasets
     """
-    full_path = f'{directory}/numerai_dataset_{napi.get_current_round()}/'
+    # Initialize Numerai's API (important)
+    napi = numerapi.NumerAPI(verbosity="info")
+    full_path = f'{data_dir}numerai_dataset_{napi.get_current_round()}/'
     train_path = full_path + 'numerai_training_data.csv'
     test_path = full_path + 'numerai_tournament_data.csv'
     train = pd.read_csv(train_path)
-    log.logger.info(msg="Train data loaded.")
+    # df_logger(msg="Train data loaded.")
     test = pd.read_csv(test_path)
-    log.logger.info(msg="Test data loaded.")
+    # df_logger(msg="Test data loaded.")
 
     if reduce_memory:
         num_features = [f for f in train.columns if f.startswith("feature")]
@@ -70,16 +101,32 @@ def load_data(directory: str, reduce_memory: bool = True) -> tuple:
     return dataset
 
 
-def get_train():
-    df = load_data(directory=out, reduce_memory=True)
+def feature_list() -> list:
+    """
+    :return: List of features of dataset
+    """
+    fl = get_train(data_dir=setup_config())
+    for group in ["intelligence", "wisdom", "charisma", "dexterity", "strength", "constitution"]:
+        cols = [col for col in fl.columns if group in col]
+        return cols
+
+
+def get_train(data_dir):
+    df = load_data(data_dir, reduce_memory=True)
     return df[0]
 
 
-def get_val():
-    df = load_data(directory=out, reduce_memory=True)
+def get_val(data_dir):
+    df = load_data(data_dir, reduce_memory=True)
     return df[1]
 
 
-def get_test():
-    df = load_data(directory=out, reduce_memory=True)
+def get_test(data_dir):
+    df = load_data(data_dir, reduce_memory=True)
     return df[2]
+
+
+conf = setup_config()
+setup_logging(log_dir=conf.get("log_directory"))
+
+
--- test/random_forest_regressor.py
+++ test/random_forest_regressor.py
@@ -3,38 +3,8 @@ import data_processing
 import pandas as pd
 
 
-def set_pd_option():
-    """
-    Set pandas dataframe output configuration
-    """
-    pd.set_option('display.max_rows', 25)
-    pd.set_option('display.min_rows', 25)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 1000)
-    pd.set_option('display.colheader_justify', 'center')
-    pd.set_option('display.precision', 2)
 
 
-def feature_list() -> list:
-    """
-    :return: List of features of dataset
-    """
-    # TODO: Add each feature to separate list
-    df = data_processing.get_train()
-    for group in ["intelligence", "wisdom", "charisma", "dexterity", "strength", "constitution"]:
-        cols = [col for col in df.columns if group in col]
-        return cols
-
-
-set_pd_option()
-
-X_train = feature_list()
-ydf = data_processing.get_train()
-ydrop = ydf.drop(labels=[''])
-# y_train =
-
-# rfr1 = RandomForestRegressor(n_estimators=1, criterion='mse', max_depth=1, max_features=1, bootstrap=True)
-# rfr1.fit(X_train, y_train)
 
 
 # TODO: clustering https://sklearn.org/modules/clustering.html
